2d01f13cc383aeeace9d3537db9e1bb871722857
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-02 21:08:12 +0545

    Passwrod field and auth component attributes

--- src/Components/InputControl/InputControl.jsx
+++ src/Components/InputControl/InputControl.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './InputControl.module.css'
+import { Eye, EyeOff } from "react-feather"
 
-function InputControl() {
+function InputControl({ label, isPassword, ...props }) {
+
+    const [isVisible, setIsVisible] = useState(true);
+
+
     return (
         <div className={styles.container}>
-            <label>Name</label>
-            <input type="text" />
+            {label && <label>{label}</label>}
+            <div className={styles.inputContainer}>
+
+                <input type={isPassword ? (isVisible ? "text" : "password") : "text"} {...props} />
+                {isPassword &&
+                    <div className={styles.icon}>
+                        {isVisible ?
+                            (<EyeOff onClick={() => setIsVisible((prev) => !prev)} />)
+                            :
+                            (<Eye onClick={() => setIsVisible((prev) => !prev)} />)}
+                    </div>
+                }
+            </div>
         </div>
     )
 }
