448a672a50d9c0c9d70dc73c528a146a59fc2650
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-02 21:09:23 +0545

    Added labels props and input controls for login/signup

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -6,10 +6,15 @@ function Auth(props) {
     const isSignup = props.signup ? true : false;
     return (
         <div className={styles.container}>
-            <p className={styles.smallLink}>{"Back to Home"}</p>
             <form className={styles.form}>
+                <p className={styles.smallLink}>{"< Back to Home"}</p>
                 <p className={styles.heading}>{isSignup ? "Signup" : "Login"}</p>
-                <InputControl />
+                <InputControl label="Name" placeholder="Enter your Name" />
+                <InputControl label="Email" placeholder="Enter your Email" />
+                <InputControl label="Passwrod" placeholder="Enter  your Password" />
+
+
+                <button>{isSignup ? "Signup" : "Login"}</button>
             </form>
         </div>
     )
