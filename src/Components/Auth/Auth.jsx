bc6f43ca63b8229f01f76bff653677587c76fe3a
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-04 20:15:05 +0545

    error msg and links to already created account/create new

--- src/Components/Auth/Auth.jsx
+++ src/Components/Auth/Auth.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import styles from './Auth.module.css'
 import InputControl from '../InputControl/InputControl';
+import { Link } from 'react-router-dom'
 
 function Auth(props) {
     const isSignup = props.signup ? true : false;
     return (
         <div className={styles.container}>
             <form className={styles.form}>
-                <p className={styles.smallLink}>{"< Back to Home"}</p>
+                <Link to='/'><p className={styles.smallLink}>{"< Back to Home"}</p></Link>
                 <p className={styles.heading}>{isSignup ? "Signup" : "Login"}</p>
-                <InputControl label="Name" placeholder="Enter your Name" />
+
+
+                {
+                    isSignup &&
+
+                    (<InputControl label="Name" placeholder="Enter your Name" />)
+                }
                 <InputControl label="Email" placeholder="Enter your Email" />
-                <InputControl label="Passwrod" placeholder="Enter  your Password" />
+                <InputControl label="Password" placeholder="Enter your Password" isPassword />
+
+                <p className={styles.error}>Error message</p>
 
 
                 <button>{isSignup ? "Signup" : "Login"}</button>
+
+                <div className={styles.bottom}>
+                    {isSignup ?
+                        (<p>Already have an account? <Link to="/login">Login here</Link></p>) :
+                        (<p>Doesnt have an account? <Link to="/signup">Create an account</Link></p>)
+                    }
+                </div>
             </form>
         </div>
     )
