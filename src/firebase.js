aa7bf026b128cfae459c71e69c47f3fe1de19f33
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-05 20:29:53 +0545

    Initialized firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCvJWlN3FUooqiZyMcIhP-N4Nwc5kjV3Gk",
+    authDomain: "project-ion-fce56.firebaseapp.com",
+    projectId: "project-ion-fce56",
+    storageBucket: "project-ion-fce56.appspot.com",
+    messagingSenderId: "594567170941",
+    appId: "1:594567170941:web:a6ebde55cce5cc2b9c1a14",
+    measurementId: "G-6EJ8P78H15"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export { app as default };
