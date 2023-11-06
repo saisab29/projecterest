436bb755448afd71e00d3d16b426ad20e3f341db
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-06 20:23:02 +0545

    getAuth()

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
 
 
 const firebaseConfig = {
@@ -15,4 +16,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app as default };
+const auth = getAuth();
+
+export { app as default, auth };
