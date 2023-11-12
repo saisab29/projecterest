6426123d6abd71892e3e186ec0ba782c0af8bee3
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-12 19:37:34 +0545

    user auth listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
 import Home from "./Components/Home/Home";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Auth from "./Components/Auth/Auth";
+import { auth } from "./firebase";
 
 
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const listener = auth.onAuthStateChanged(user => {
+      console.log(user)
+    })
+
+    return () => listener();
+  }, [])
   return (
     <div className="App">
       <Router>
