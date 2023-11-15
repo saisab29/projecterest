bc69c3393b3401c9be16aec0735b15360370f5bd
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-15 20:58:16 +0545

    Loader added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,32 @@
 
 import Home from "./Components/Home/Home";
+import Loader from "./Components/Loader/Loader";
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom"
 import Auth from "./Components/Auth/Auth";
 import { auth, getUserFromDatabase } from "./firebase";
+import "./App.css";
+
 
 
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [userDetails, setUserDetails] = useState({});
+  const [isDataLoaded, setIsDataLoaded] = useState(false);
 
 
   const fetchUserDetails = async (uid) => {
     const userDetails = await getUserFromDatabase(uid);
+    setIsDataLoaded(true);
     setUserDetails(userDetails);
   }
   useEffect(() => {
     const listener = auth.onAuthStateChanged(user => {
-      if (!user) return;
+      if (!user) {
+        setIsDataLoaded(true);
+        return;
+      }
 
       setIsAuthenticated(true);
       fetchUserDetails(user.uid)
@@ -30,22 +38,27 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          {!isAuthenticated && (<>
+        {isDataLoaded ?
+
+          (<Routes>
+            {!isAuthenticated && (<>
 
 
-            <Route path="/login" element={<Auth />} />
-            <Route path="/signup" element={<Auth signup />} />
-          </>
+              <Route path="/login" element={<Auth />} />
+              <Route path="/signup" element={<Auth signup />} />
+            </>
 
-          )}
+            )}
 
-          <Route path="/account" element={<h1>Login</h1>} />
-          <Route path="/" element={<Home auth={isAuthenticated} />} />
-          <Route path="/*" element={<Navigate to="/" />} />
+            <Route path="/account" element={<h1>Login</h1>} />
+            <Route path="/" element={<Home auth={isAuthenticated} />} />
+            <Route path="/*" element={<Navigate to="/" />} />
 
 
-        </Routes>
+          </Routes>
+          ) : (<div className="loader"><Loader /></div>)
+
+        }
       </Router>
 
 
