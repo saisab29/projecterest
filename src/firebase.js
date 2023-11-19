955fcaf6cd98a246e25337afdc6caba713ec718c
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-19 21:12:03 +0545

    upload profile image of user to firebase storage

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { doc, setDoc, getDoc, getFirestore } from 'firebase/firestore'
-import { getStorage } from 'firebase/storeage'
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCvJWlN3FUooqiZyMcIhP-N4Nwc5kjV3Gk",
@@ -38,6 +38,43 @@ const getUserFromDatabase = async (uid) => {
 
 
 }
+const uploadImage = (file, progressCallback, urlCallback, errorCallback) => {
+    if (!file) {
+        errorCallback("File not found");
+        return;
+    }
+    const fileType = file.type;
+    const fileSize = file.size / 1024 / 1024;
 
 
-export { app as default, auth, db, updateUserToDatabase, getUserFromDatabase };
+    if (!fileType.includes("image")) {
+        errorCallback("File must be an image");
+        return
+
+    }
+    if (fileSize > 2) {
+        errorCallback("File must be smaller than 2MB");
+        return;
+    }
+
+    const storageRef = ref(storage, `images/${file.name}`);
+
+    const task = uploadBytesResumable(storageRef, file);
+    task.on('state_changed', (snapshot) => {
+        const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        progressCallback(progress)
+
+    }, error => {
+        errorCallback(error.message);
+    }, () => {
+        getDownloadURL(storageRef).then((url) => {
+            urlCallback(url);
+        })
+
+    })
+
+
+}
+
+
+export { app as default, auth, db, updateUserToDatabase, getUserFromDatabase, uploadImage };
