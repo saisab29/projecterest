3bd571f3afb56c87c842a275a270df7833097cdb
saisab29 <107755209+saisab29@users.noreply.github.com>
2023-11-24 20:43:54 +0545

    taking username from userProfileValues instead

--- src/Components/Account/Account.jsx
+++ src/Components/Account/Account.jsx
@@ -78,7 +78,7 @@ function Account(props) {
     <div className={styles.container}>
       <div className={styles.header}>
         <p className={styles.heading}>
-          Bonjour, <span>{userDetails.name}</span>!
+          Bonjour, <span>{userprofileValues.name}</span>!
         </p>
         <div className={styles.logout} onClick={handleLogout}>
           <LogOut /> Logout
